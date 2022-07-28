4b3521029d8fd1d522a02f6289f8a12f5d516ede
AlekUp24 <53192697+AlekUp24@users.noreply.github.com>
2022-07-28 10:39:43 +0200

    Python read, change and write data table to Excel
    cosmetic changes

--- ExcelDemo.py
+++ ExcelDemo.py
@@ -8,8 +8,8 @@ ws = "testowa"
 
 #utwórz i zapisz w Excelu
 df = pd.DataFrame({'States':['California', 'Florida', 'Montana', 'Colorodo', 'Washington', 'Virginia'],
-    'Capitals':['Sacramento', 'Tallahassee', 'Helena', 'Denver', 'Olympia', 'Richmond'],
-    'Population':['508529', '193551', '32315', '619968', '52555', '227032']})
+                   'Capitals':['Sacramento', 'Tallahassee', 'Helena', 'Denver', 'Olympia', 'Richmond'],
+                   'Population':['508529', '193551', '32315', '619968', '52555', '227032']})
 
 df.to_excel(pathT, sheet_name=ws, index = False)
 
